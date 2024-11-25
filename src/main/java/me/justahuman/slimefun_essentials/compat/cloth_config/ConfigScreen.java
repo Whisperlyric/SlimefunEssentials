a6da3b7fc4e824187eecb83cc5df19f0c4bcc978
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2024-11-25 12:06:18 -0600

    Update ConfigScreen.java

--- src/main/java/me/justahuman/slimefun_essentials/compat/cloth_config/ConfigScreen.java
+++ src/main/java/me/justahuman/slimefun_essentials/compat/cloth_config/ConfigScreen.java
@@ -20,7 +20,6 @@ public class ConfigScreen {
         final ConfigEntryBuilder entryBuilder = builder.entryBuilder();
         final ConfigCategory generalCategory = builder.getOrCreateCategory(Text.translatable("slimefun_essentials.config.category.general"));
         final ConfigCategory visualCategory = builder.getOrCreateCategory(Text.translatable("slimefun_essentials.config.category.visual"));
-        final ConfigCategory serverCategory = builder.getOrCreateCategory(Text.translatable("slimefun_essentials.config.category.server"));
 
         /* General Config Options */
 
@@ -31,24 +30,11 @@ public class ConfigScreen {
                 .setSaveConsumer(ModConfig::setBlockFeatures)
                 .build());
 
-        /*generalCategory.addEntry(entryBuilder.startBooleanToggle(Text.translatable("slimefun_essentials.config.option.custom_guide"), ModConfig.customGuide())
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.custom_guide.tooltip"))
-                .setRequirement(CompatUtils::isPatchouliLoaded)
-                .setSaveConsumer(ModConfig::setCustomGuide)
-                .build());*/
-
         generalCategory.addEntry(entryBuilder.startBooleanToggle(Text.translatable("slimefun_essentials.config.option.recipe_features"), ModConfig.recipeFeatures())
                 .setDefaultValue(true)
                 .setTooltip(Text.translatable("slimefun_essentials.config.option.recipe_features.tooltip"))
                 .setRequirement(CompatUtils::isRecipeModLoaded)
                 .setSaveConsumer(ModConfig::setRecipeFeatures)
-                .build());
-
-        generalCategory.addEntry(entryBuilder.startStrList(Text.translatable("slimefun_essentials.config.option.addons"), ModConfig.getAddons())
-                .setDefaultValue(new ArrayList<>(List.of("Slimefun", "InfinityExpansion")))
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.addons.tooltip"))
-                .setSaveConsumer(ModConfig::setAddons)
                 .build());
 
         /* Visual */
@@ -63,38 +49,6 @@ public class ConfigScreen {
                 .setDefaultValue(true)
                 .setTooltip(Text.translatable("slimefun_essentials.config.option.hide_background_tooltips.tooltip"))
                 .setSaveConsumer(ModConfig::setHideBackgroundTooltips)
-                .build());
-
-        /* Server Config Options */
-
-        serverCategory.addEntry(entryBuilder.startBooleanToggle(Text.translatable("slimefun_essentials.config.option.require_server_connection"), ModConfig.requireServerConnection())
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.require_server_connection.tooltip"))
-                .setSaveConsumer(ModConfig::setRequireServerConnection)
-                .build());
-
-        serverCategory.addEntry(entryBuilder.startBooleanToggle(Text.translatable("slimefun_essentials.config.option.enable_server_whitelist"), ModConfig.enableServerWhitelist())
-                .setDefaultValue(false)
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.enable_server_whitelist.tooltip"))
-                .setSaveConsumer(ModConfig::setEnableServerWhitelist)
-                .build());
-
-        serverCategory.addEntry(entryBuilder.startStrList(Text.translatable("slimefun_essentials.config.option.server_whitelist"), ModConfig.getServerWhitelist())
-                .setDefaultValue(new ArrayList<>())
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.server_whitelist.tooltip"))
-                .setSaveConsumer(ModConfig::setServerWhitelist)
-                .build());
-
-        serverCategory.addEntry(entryBuilder.startBooleanToggle(Text.translatable("slimefun_essentials.config.option.auto_toggle_addons"), ModConfig.autoToggleAddons())
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.auto_toggle_addons.tooltip"))
-                .setSaveConsumer(ModConfig::setAutoToggleAddons)
-                .build());
-
-        serverCategory.addEntry(entryBuilder.startBooleanToggle(Text.translatable("slimefun_essentials.config.option.auto_manage_items"), ModConfig.autoManageItems())
-                .setDefaultValue(true)
-                .setTooltip(Text.translatable("slimefun_essentials.config.option.auto_manage_items.tooltip"))
-                .setSaveConsumer(ModConfig::setAutoManageItems)
                 .build());
 
         builder.setSavingRunnable(ModConfig::saveConfig);
